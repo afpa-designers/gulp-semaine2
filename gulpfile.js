d59ef66cb3b780a5ddcb614fec59e5fb9452643c
TomDelalbre <thomas.delalbre@gmail.com>
2020-03-24 09:35:00 +0100

    updating scss and gulp process

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,34 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass'); // => Pour pouvoir charger 'gulp-sass' il faut l'installer via la console
 // installer gulp-sass via la console : npm install --save-dev gulp-sass
+
+var minify = require('gulp-minify'); // Minify JS files
+var cleanCSS = require('gulp-clean-css'); // Crée un fichier minifié CSS
+
+
 // Warning ! Cette installation est à réaliser pour chaque plugin supplémentaire (voir doc du plugin en question) !!! 
 
-var minify = require('gulp-minify'); 
+
+
 
 
 // Notre script : la liste et la définition des tâches que l'on veut effectuer
 
 // Tache 1 : 'scss' => compiler le SCSS en CSS :
 gulp.task('scss', (done) => {
-    gulp.src('./assets/scss/bootstrap.scss') // Source du fichier que nous voulons traduire en css
-      .pipe(sass())                      // Appelle de du module sass, qui s'occupe de traduire notre scss en css
+    gulp.src('./assets/scss/main.scss') // Source du fichier que nous voulons traduire en css
+      .pipe(sass()) // passer en CSS
+      .pipe(cleanCSS({debug: true}, (details) => { //minifier le CSS
+        console.log(`${details.name}: ${details.stats.originalSize}`);
+        console.log(`${details.name}: ${details.stats.minifiedSize}`);
+      }))                      // Appelle de du module sass, qui s'occupe de traduire notre scss en css
       .pipe(gulp.dest('./dist/css/'))   // Chemin de destination du fichier une fois compilé en css
       // .pipe(livereload())
       done();
 });
 
 
-gulp.task('minifyJS', (done) => {
+gulp.task('minifyJS', (done) => { // gérer les fichiers JS
     gulp.src('./assets/js/*.js')
       .pipe(minify())
       .pipe(gulp.dest('./dist/js/'))
@@ -30,7 +40,6 @@ gulp.task('minifyJS', (done) => {
 
 
 // Fonctions de lancement qui execute nos tâches dans un précis
-
 gulp.task('default', gulp.series('scss', 'minifyJS'));
 
 
